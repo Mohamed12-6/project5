26e400513197d18eef2fe6004263d266c35dfd6f
Mohamed Osama <mohamedosama01005045063@gmail.com>
2024-02-25 12:25:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './App.css';
+import { Header } from './Components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Banner } from './Components/Banner';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import MessEmail from './Components/MessEmail';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Banner/>
+        <Skills/>
+        <Projects/>
+        <Contact/>
+        <MessEmail/>
+        <Footer/>
+
+
     </div>
   );
 }
